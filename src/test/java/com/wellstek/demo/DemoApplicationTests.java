2fc5aa37def8675fd15ec31b35df10a380baaaed
vingorius <vingorius@gmail.com>
2024-05-21 15:38:56 +0900

    first commit

---
+++ src/test/java/com/wellstek/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellstek.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
