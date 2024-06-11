42aed005b3572c9417fe2a8e3e56175f6566a954
vingorius <vingorius@gmail.com>
2024-06-11 14:34:45 +0900

    add demoapplication testsuit

--- src/test/java/com/wellstek/demo/DemoApplicationTests.java
+++ src/test/java/com/wellstek/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.wellstek.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private MainController mainController;
+
 	@Test
 	void contextLoads() {
+		assertThat(mainController).isNotNull();
 	}
 
 }
