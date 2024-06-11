4538a4286430bb5b4527038a4651ca0cccec9803
vingorius <vingorius@gmail.com>
2024-06-11 14:43:28 +0900

    make security hotspot(random)

--- src/main/java/com/wellstek/demo/SecondController.java
+++ src/main/java/com/wellstek/demo/SecondController.java
@@ -1,12 +1,18 @@
 package com.wellstek.demo;
 
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecondController {
+    Random random = new Random(); // Sensitive use of Random
+    byte bytes[] = new byte[20];
+
     @GetMapping("/second")
     public String second() {
+        random.nextBytes(bytes); // Check if bytes is used for hashing, encryption, etc...
         return "Second Hello World!";
     }
 }
